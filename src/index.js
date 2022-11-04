81a83f407fd291ef1306f2699c8c1c08a5b2cf9e
Grace Lee <gracexsnoopy@gmail.com>
2022-11-04 14:44:23 -0700

    Modify printElements function and remove create element methods

--- src/index.js
+++ src/index.js
@@ -13,31 +13,16 @@ async function getKanji(response) {
 }
 
 function printElements(data) {
-  const ul = document.createElement('ul');
-  const li1 = document.createElement('li');
-  const li2 = document.createElement('li');
-  const li3 = document.createElement('li');
-  const li4 = document.createElement('li');
-  const li5 = document.createElement('li');
-  const li6 = document.createElement('li');
-  const p = document.getElementById('result');
-  li1.append(data.kanji);
-  li2.append(data.stroke_count);
-  li3.append(data.meanings.join(', '));
-  li4.append(data.kun_readings.join(', '));
-  li5.append(data.on_readings.join(', '));
-  li6.append(data.jlpt);
-  ul.append(li1);
-  ul.append(li2);
-  ul.append(li3);
-  ul.append(li4);
-  ul.append(li5);
-  ul.append(li6);
-  p.append(ul);
+  document.getElementById("kanji-character").innerText = data.kanji;
+  document.getElementById("stroke-count").innerText = data.stroke_count;
+  document.getElementById("meaning").innerText = data.meanings.join(', ');
+  document.getElementById("kun-yomi").innerText = data.kun_readings.join(', ');
+  document.getElementById("on-yomi").innerText = data.on_readings.join(', ');
+  document.getElementById("jlpt-level").innerText = data.jlpt;
 }
 
 function printError(error) {
-  document.getElementById('result').innerText = error;
+  document.getElementsByClassName('error').innerText = error;
 }
 
 function handleFormSubmission(event) {
