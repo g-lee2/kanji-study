959520352b56e695ae6e14d02be044c3575267d3
Grace Lee <gracexsnoopy@gmail.com>
2022-11-04 13:58:35 -0700

    Revise printElements function to print certain data

--- src/index.js
+++ src/index.js
@@ -1,32 +1,49 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import KanjiService from './kanji.js';
+import KanjiService from './js/kanji.js';
 
-// Business Logic
-
-function getKanji() {
-  let promise = KanjiService.getKanji();
-  promise.then(function(kanjiDataArray) {
-    printElements(kanjiDataArray);
-  }, function(errorArray) {
-    printError(errorArray);
-  });
+async function getKanji(response) {
+  const responseAPI = await KanjiService.getKanji(response);
+  if (responseAPI) {
+    printElements(responseAPI);
+  } else {
+    printError(responseAPI);
+  }
 }
 
-// UI Logic
-
 function printElements(data) {
-  document.getElementById('Response').innerText = data;
+  const ul = document.createElement('ul');
+  const li1 = document.createElement('li');
+  const li2 = document.createElement('li');
+  const li3 = document.createElement('li');
+  const li4 = document.createElement('li');
+  const li5 = document.createElement('li');
+  const li6 = document.createElement('li');
+  const p = document.getElementById('result');
+  li1.append(data.kanji);
+  li2.append(data.stroke_count);
+  li3.append(data.meanings.join(', '));
+  li4.append(data.kun_readings.join(', '));
+  li5.append(data.on_readings.join(', '));
+  li6.append(data.jlpt);
+  ul.append(li1);
+  ul.append(li2);
+  ul.append(li3);
+  ul.append(li4);
+  ul.append(li5);
+  ul.append(li6);
+  p.append(ul);
 }
 
 function printError(error) {
-  document.getElementById('Response').innerText = error;
+  document.getElementById('result').innerText = error;
 }
 
 function handleFormSubmission(event) {
   event.preventDefault();
-  getKanji();
+  const inputKanji = document.getElementById('kanji').value;
+  getKanji(inputKanji);
 }
 
 window.addEventListener("load", function() {
